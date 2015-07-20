56bcf6d6684f8694a479c8990c9cdf5fe1c6e170
wizsid11 <siddharth.mahapatra95@gmail.com>
2015-07-21 02:14:18 +0530

    Making Changes

--- Competitive Coding/SPOJ_Life_the_Universe_and_Everything.cpp
+++ Competitive Coding/SPOJ_Life_the_Universe_and_Everything.cpp
@@ -42,11 +42,12 @@ using namespace std;
 
 int main(){
 
-	int a;
+	
 	while (1){
-		scanf("%d", &a);
-		if (a!=42){
-			printf("%d\n", a);
+		int n;
+		scanf("%d", &n);
+		if (n!=42){
+			printf("%d\n", n);
 		}
 		else
 			break;
